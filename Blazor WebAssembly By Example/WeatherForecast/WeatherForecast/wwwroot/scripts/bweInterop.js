542a861c334874da36c5c45891789648ecf8b2f3
KRob314 <krob636@gmail.com>
2021-08-28 13:14:33 -0700

    weather forecast PWA

---
+++ Blazor WebAssembly By Example/WeatherForecast/WeatherForecast/wwwroot/scripts/bweInterop.js
@@ -0,0 +1,41 @@
+﻿var bweInterop = {};
+
+bweInterop.getPosition = async function ()
+{
+    function getPositionAsync()
+    {
+        return new Promise((success, error) =>
+        {
+            navigator.geolocation.getCurrentPosition(success, error);
+        });
+    }
+
+    if (navigator.geolocation)
+    {
+        var position = await getPositionAsync();
+        var coords = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+        return coords;
+    }
+    else
+    {
+        throw Error("Geolocation is not supported by this browser");
+    }
+}
+
+bweInterop.getWeatherForecastKey = async function ()
+{
+    return config.WeatherForecast_Key;
+}
+
+bweInterop.setLocalStorage = function (key, data)
+{
+    localStorage.setItem(key, data);
+}
+
+bweInterop.getLocalStorage = function (key)
+{
+    return localStorage.getItem(key);
+}
---
+++ Blazor WebAssembly By Example/WeatherForecast/WeatherForecast/wwwroot/service-worker.js
@@ -0,0 +1,75 @@
+﻿const OFFLINE_VERSION = 1.1;
+const CACHE_PREFIX = 'offline';
+const CACHE_NAME = `${CACHE_PREFIX}${OFFLINE_VERSION}`;
+const OFFLINE_URL = 'offline.html';
+const urlsToCache = [
+    '/',
+    '/css/bootstrap/bootstrap.min.css',
+    '/css/app.css',
+    '/WeatherForecast.styles.css',
+    '/css/open-iconic/font/css/open-iconic-bootstrap.min.css',
+    '/scripts/bweInterop.js',
+    '/_framework/blazor.webassembly.js',
+    '/_framework/dotnet.5.0.9.js'
+];
+
+self.addEventListener('install', event => event.waitUntil(onInstall(event)));
+
+self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
+
+self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+
+async function onInstall(event)
+{
+    console.info('Service worker: Install');
+    const cache = await caches.open(CACHE_NAME);
+
+    await cache.add(new Request(OFFLINE_URL));
+
+    //event.waitUntil(
+    //    caches.open(CACHE_NAME)
+    //        .then(function (cache)
+    //        {
+    //            console.log('Opened cache');
+    //            return cache.addAll(urlsToCache);
+    //        })
+    //);
+}
+
+async function onActivate(event)
+{
+    console.info('Service worker: Activate');
+    const cacheKeys = await caches.keys();
+
+    await Promise.all(cacheKeys.filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME).map(key => caches.delete(key)));
+
+}
+
+async function onFetch(event)
+{
+    if (event.request.method === 'GET')
+    {
+        try
+        {
+            return await fetch(event.request);
+            //event.respondWith(
+            //    caches.match(event.request)
+            //        .then(function (response)
+            //        {
+            //            // Cache hit - return response
+            //            if (response)
+            //            {
+            //                return response;
+            //            }
+            //            return fetch(event.request);
+            //        })
+            //);
+        }
+        catch (error)
+        {
+            const cache = await caches.open(CACHE_NAME);
+
+            return await cache.match(OFFLINE_URL);
+        }
+    }
+}
