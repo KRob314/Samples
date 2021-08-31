5343f16a61b43a51c4e10b9e5ac622a09b0c3891
KRob314 <krob636@gmail.com>
2021-08-30 21:27:54 -0700

    shopping cart

---
+++ Blazor WebAssembly By Example/ShoppingCart/ShoppingCart/Services/CartService.cs
@@ -0,0 +1,38 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Services
+{
+    public class CartService : ICartService
+    {
+        private void NotifyStateChanged() => OnChange?.Invoke();
+        public IList<Product> Cart { get; private set; }
+        public int Total { get; set; }
+
+        public event Action OnChange;
+
+        public CartService()
+        {
+            Cart = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            Cart.Add(product);
+            Total += product.Price;
+            NotifyStateChanged();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            Cart.Remove(product);
+            Total -= product.Price;
+            NotifyStateChanged();
+        }
+
+
+    }
+}
