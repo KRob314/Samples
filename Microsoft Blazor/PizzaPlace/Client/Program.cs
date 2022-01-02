9fc91fd6032832e2fa3909e26fa4b9a75052a403
KRob314 <krob636@gmail.com>
2022-01-02 00:00:05 -0800

    Services and Dependency Injection
    Services and Dependency Injection

--- Microsoft Blazor/PizzaPlace/Client/Program.cs
+++ Microsoft Blazor/PizzaPlace/Client/Program.cs
@@ -1,11 +1,31 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using PizzaPlace.Client;
+using PizzaPlace.Shared;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace PizzaPlace.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+            builder.Services.AddTransient<IMenuService, HardCodedMenuService>();
+            builder.Services.AddTransient<IOrderService, ConsoleOrderService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
+
+
+
